254e7f31a4132f2974312cd711619ced0e48263c
Василий <kochanvasia@mail.ru>
2022-08-19 13:01:12 +0300

    Added city details

--- src/cities/urls.py
+++ src/cities/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_cities, name='home')
+    path('', views.show_cities, name='city_home'),
+    path('<int:pk>/', views.show_cities, name='city_details'),
 ]
--- src/cities/views.py
+++ src/cities/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from .models import City
 
 
-def show_cities(request):
+def show_cities(request, pk=None):
+    if pk:
+        city = City.objects.filter(id=pk).first()
+        context = {
+            'city': city
+        }
+        return render(request, 'cities/details.html', context=context)
     qs = City.objects.all()
     context = {
         'qs': qs
     }
     return render(request, 'cities/home.html', context=context)
-
