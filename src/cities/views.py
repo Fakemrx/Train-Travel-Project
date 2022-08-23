ff4a7ee7f020cf9fcc20bdacb5c25aff112f805f
Василий <kochanvasia@mail.ru>
2022-08-23 12:51:31 +0300

    Messages 1) Added error messages for add/update/delete actions 2) Added warning messages for delete action 3) Added success messages for update/add actions

--- src/cities/views.py
+++ src/cities/views.py
@@ -1,3 +1,4 @@
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
@@ -7,7 +8,7 @@ from .models import City
 
 
 class ContactListView(ListView):
-    paginate_by = 3
+    paginate_by = 10
     model = City
     template_name = 'cities/home.html'
 
@@ -16,19 +17,23 @@ class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
 
-class CityCreateView(CreateView):
+
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
+    success_message = 'Вы добавили город'
 
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
+    success_message = 'Вы обновили город'
 
 
-class CityDeleteView(DeleteView):
+class CityDeleteView(SuccessMessageMixin, DeleteView):
     model = City
     template_name = 'cities/delete.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Вы удалили город'
